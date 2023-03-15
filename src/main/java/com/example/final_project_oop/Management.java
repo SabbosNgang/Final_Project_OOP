9e47ee7764bb2fb3f76991b298a17232cb1dfde4
Heaklinh <heaklinh@gmail.com>
2023-03-15 22:12:58 +0700

    main screen

--- src/main/java/com/example/final_project_oop/Management.java
+++ src/main/java/com/example/final_project_oop/Management.java
@@ -1,4 +1,24 @@
 package com.example.final_project_oop;
 
-public class Management {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Management extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management.class.getResource("healthManage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
