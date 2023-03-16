50bf030983712bfee71745c7ed8513bdc73683f5
Heaklinh <heaklinh@gmail.com>
2023-03-16 12:09:00 +0700

    update

---
+++ src/main/java/com/example/final_project_oop/BookController.java
@@ -0,0 +1,6 @@
+package com.example.final_project_oop;
+
+
+public class BookController {
+
+}
---
+++ src/main/java/com/example/final_project_oop/Management_Controller.java
@@ -0,0 +1,61 @@
+package com.example.final_project_oop;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class Management_Controller {
+    @FXML
+    private Button btn1, btn2, btn3, btn4, btn5;
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private void onBookClick(ActionEvent event){
+
+    }
+    @FXML
+    private void onDrugClick(ActionEvent event){
+
+    }
+    @FXML
+    private void onCalenderClick(ActionEvent event){
+
+    }
+
+    public void onHomeClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management_Main.class.getResource("home.fxml"));
+        borderPane.getChildren().remove(borderPane.getCenter()); ////remove existing fxml from center.
+        borderPane.setCenter(fxmlLoader.load());
+    }
+
+
+    public void onBookClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management_Main.class.getResource("book.fxml"));
+        borderPane.getChildren().remove(borderPane.getCenter()); ////remove existing fxml from center.
+        borderPane.setCenter(fxmlLoader.load());
+    }
+
+    public void onDrugClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management_Main.class.getResource("pharmacy.fxml"));
+        borderPane.getChildren().remove(borderPane.getCenter()); ////remove existing fxml from center.
+        borderPane.setCenter(fxmlLoader.load());
+    }
+
+    public void onCalenderClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management_Main.class.getResource("calender.fxml"));
+        borderPane.getChildren().remove(borderPane.getCenter()); ////remove existing fxml from center.
+        borderPane.setCenter(fxmlLoader.load());
+    }
+
+    public void onProfileClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Management_Main.class.getResource("profile.fxml"));
+        borderPane.getChildren().remove(borderPane.getCenter()); ////remove existing fxml from center.
+        borderPane.setCenter(fxmlLoader.load());
+    }
+}
